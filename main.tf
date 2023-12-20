32e0c281bd6a7960bd49512ce0e87df18fcad0bd
sriram <munukutla2001@gmail.com>
2023-12-20 07:37:29 +0530

    project

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
- module "test" {
-   source = "git::https://github.com/Suryakumari76k/robo_terraform_module.git"
-   env = var.env
- }
+module "instance" {
+  for_each = var.component
+  source = "Git::https://github.com/Suryakumari76k/robo_terraform_module.git"
+  env = var.env
+  component = each.key
+}
