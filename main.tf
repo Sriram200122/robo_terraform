8fc1f6f7708bb87946b814b5b6953638f160303b
sriram <munukutla2001@gmail.com>
2023-12-20 10:39:08 +0530

    project

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "instance" {
   for_each = var.component
-  source = "Git::https://github.com/Suryakumari76k/robo_terraform_module.git"
+  source = "git::https://github.com/Suryakumari76k/robo_terraform_module.git"
   env = var.env
   component = each.key
 }
